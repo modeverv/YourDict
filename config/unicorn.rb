669bcd0d34490bf6dd1056ecfae5c3e63f64e20a
modeverv <modeverv@gmail.com>
2013-08-27 00:20:25 +0900

    heroku8

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,14 +1,14 @@
 worker_processes 1
 
-before_fork do |server, worker|
-   @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
-end
-
-after_fork do |server, worker|
-  Sidekiq.configure_client do |config|
-    config.redis = { :size => 1 }
-  end
-  Sidekiq.configure_server do |config|
-    config.redis = { :size => 1 }
-  end
-end
+#before_fork do |server, worker|
+#   @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
+#end
+#
+#after_fork do |server, worker|
+#  Sidekiq.configure_client do |config|
+#    config.redis = { :size => 1 }
+#  end
+#  Sidekiq.configure_server do |config|
+#    config.redis = { :size => 1 }
+#  end
+#end
